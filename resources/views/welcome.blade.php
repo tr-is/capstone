256768b10b4613caa034b1bfc7a64cb5dca20e4b
Kewal Lamichhane <s3559695@student.rmit.edu.a>
2018-08-20 17:42:31 +1000

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravehntnt
+                    LaravehTest
                 </div>
 
                 <div class="links">
