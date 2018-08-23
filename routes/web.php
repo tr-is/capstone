d6a24e3ba974c13385d6f5aa4c4b233a426e8402
Kewal Lamichhane <s3559695@student.rmit.edu.a>
2018-08-23 22:33:14 +1000

    routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,29 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('employer')->group(function() {
+    Route::get('/register', 'Auth\AdminRegisterController@showRegistrationForm')->name('admin.register.form');
+    Route::post('/register', 'Auth\AdminRegisterController@register')->name('admin.register');
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+    Route::get('/', 'AdminController@index')->name('admin.home');
+
+    /**
+     * Admin Job routes
+     */
+    Route::get('/job/list', 'Admin\JobController@listJobs')->name('admin.job.list');
+    Route::any('/job/create', 'Admin\JobController@create')->name('admin.job.create');
+    Route::any('/job/{id}/update', 'Admin\JobController@create')->name('admin.job.update');
+    Route::any('/job/{id}/delete', 'Admin\JobController@delete')->name('admin.job.delete');
 });
