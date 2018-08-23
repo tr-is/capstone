06f8730a643ed77f2a2a66418d4183f3e5e6ba5e
Kewal Lamichhane <s3559695@student.rmit.edu.a>
2018-08-24 05:13:33 +1000

    utf to general

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'password' => env('DB_PASSWORD', 'way2kewal'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'collation' => 'utf8mb4_general_ci',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
