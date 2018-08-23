3de002262e2b3ff95be00dbc2acd9529b767ea0a
Kewal Lamichhane <s3559695@student.rmit.edu.a>
2018-08-24 05:58:32 +1000

    no idea why it isnt working

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::defaultStringLength(191);
+	Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email');
