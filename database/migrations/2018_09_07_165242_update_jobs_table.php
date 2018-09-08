0c3fd7a98ba5c14bfcc56f8fbadd6661e33ea903
Kewal Lamichhane <26236473+s3559695@users.noreply.github.com>
2018-09-09 00:03:51 +0545

    update jobs table

---
+++ database/migrations/2018_09_07_165242_update_jobs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('jobs', function(Blueprint $table){
+            $table->string('experience')->nullable();
+            $table->string('education')->nullable();
+            $table->string('salary_range')->nullable();
+            $table->string('skills_required')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('jobs', function(Blueprint $table){
+            $table->dropColumn([
+                'experience',
+                'education',
+                'salary_range',
+                'skills_required'
+            ]);
+        });
+    }
+}
