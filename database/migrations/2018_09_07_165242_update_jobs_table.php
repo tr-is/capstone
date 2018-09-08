20b0d51e85e2b7eb42d9d504fbc49d78afc2f6d8
Kewal Lamichhane <26236473+s3559695@users.noreply.github.com>
2018-09-09 00:04:45 +0545

    Update 2018_09_07_165242_update_jobs_table.php

--- database/migrations/2018_09_07_165242_update_jobs_table.php
+++ database/migrations/2018_09_07_165242_update_jobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateAdminsTable extends Migration
+class UpdateJobsTable extends Migration
 {
     /**
      * Run the migrations.
