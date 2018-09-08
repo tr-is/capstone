c4790356921b6a036714271bf280320f3804ab7e
Kewal Lamichhane <26236473+s3559695@users.noreply.github.com>
2018-09-09 00:07:10 +0545

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Job;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -23,4 +27,20 @@ class UserController extends Controller
         $jobs = Job::take(10)->get();
         return view('home', compact('jobs'));
     }
+
+    public function update(Request $request){
+        $user = Auth::user();
+        if($request->getMethod() == 'POST'){
+            $input = $request->input();
+            $user->fill(Arr::except($input,['password', 'password_confirmation']));
+            if(isset($input['password'])){
+                $user->password = Hash::make($input['password']);
+            }
+            $user->save();
+            return redirect()->back()->withInput()->with('success', 'Profile updated.');
+        }   else    {
+            return view('user.update', compact('user'));
+        }
+    }
+
 }
