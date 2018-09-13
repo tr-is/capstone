810af13e394961663378e6de5152590f88eb73e5
chiranyarao <chiranya.rao@gmail.com>
2018-09-13 13:28:08 +0545

    table fix

--- resources/views/user/jobs/appliedjob.blade.php
+++ resources/views/user/jobs/appliedjob.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="container">
     <div class="col-md-12">
         
             <table class="table table-bordered table-condensed table-striped table-hover">
@@ -10,17 +10,16 @@
                     <th>List of job applied</th>
                 </tr>
                 </thead>
-                <tbody>
-                
                 @foreach($jobs as $job)
-
+                <tbody>
                    <td>
-                    <h4><a href="{{ route('job.detail.frontend',['slug' => $job->slug]) }}">{{ $job->title }}</a></h4></td>
-                @endforeach
+                        <a href="{{ route('job.detail.frontend',['slug' => $job->slug]) }}">{{ $job->title }}</a>
+                    </td>
                 </tbody>
+                @endforeach
             </table>
               
-        
+    </div>
     </div>
 
 @endsection
