d8df56d5320bd10054a2aef89a8c3932187b763e
chiranyarao <chiranya.rao@gmail.com>
2018-09-13 12:25:48 +0545

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
+    <!-- Styles -->
 @section('styles')
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-    <!-- Styles -->
     <style>
         html, body {
             background-color: #fff;
@@ -18,40 +18,99 @@
 @endsection
 
 @section('content')
-    <div class="container">
-        <div class="search-block">
+<!-- search job starts -->
+    <div class="searchwrap">
+        <div class="container">
+            <h3>One million success stories. <span>Start yours today.</span></h3>
             <form>
-                <div class="input-group mb-3 col-6 offset-3">
-                    <input type="text"
-                           name="query"
-                           class="form-control"
-                           placeholder="Search text"
-                           value="{{ request('query') }}"
-                           aria-label="Recipient's username"
-                           aria-describedby="basic-addon2">
-                    <div class="input-group-append">
-                        <button class="btn btn-outline-secondary" type="submit">Search</button>
+                <div class="searchbar row">
+                    <div class="col-md-3">
+                    </div>
+                    <div class="col-md-5">
+                        <input type="text" class="form-control" value="{{ request('query') }}" placeholder="Enter Skills or job title">
+                    </div>
+                    <div class="col-md-2">
+                        <input type="submit" class="btn" value="Search Job">
                     </div>
                 </div>
-            </form>
-        </div>
-        <hr>
-        <div class="jobs-list-block">
-            <div class="mb-3 col-6 offset-3">
-                <div class="card">
-                    <div class="card-header text-center">Job List</div>
-                    <ul class="list-group list-group-flush">
-                        @foreach($jobs as $job)
-                            <li class="list-group-item">
-                                <span class="float-left">{{ $job->title }}</span>
-                                <span class="float-right">
-                                    <a href="{{ route('job.detail.frontend',['slug' => $job->slug]) }}">View</a>
-                                </span>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
+            </form> 
         </div>
     </div>
+
+<!-- search jobs ends-->
+
+<!-- How it Works start -->
+<div class="section howitwrap">
+  <div class="container"> 
+    <!-- title start -->
+    <div class="titleTop">
+      <div class="subtitle">Here You Can See</div>
+      <h3>How It <span>Works?</span></h3>
+    </div>
+    <!-- title end -->
+    <ul class="howlist row">
+      <!--step 1-->
+      <li class="col-md-4 col-sm-4">
+        <div class="iconcircle"><i class="fa fa-user" aria-hidden="true"></i></div>
+        <h4>Create An Account</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
+      </li>
+      <!--step 1 end--> 
+      
+      <!--step 2-->
+      <li class="col-md-4 col-sm-4">
+        <div class="iconcircle"><i class="fa fa-black-tie" aria-hidden="true"></i></div>
+        <h4>Search Desired Job</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
+      </li>
+      <!--step 2 end--> 
+      
+      <!--step 3-->
+      <li class="col-md-4 col-sm-4">
+        <div class="iconcircle"><i class="fa fa-file-text" aria-hidden="true"></i></div>
+        <h4>Send Your Resume</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
+      </li>
+      <!--step 3 end-->
+    </ul>
+  </div>
+</div>
+<!-- How it Works Ends --> 
+
+<!-- Latest Jobs start -->
+<div class="section greybg">
+  <div class="container"> 
+    <!-- title start -->
+    <div class="titleTop">
+      <div class="subtitle">Here You Can See</div>
+      <h3>Latest <span>Jobs</span></h3>
+    </div>
+    <!-- title end -->
+    
+    <ul class="jobslist row">
+      <!--Job 1-->
+      <li class="col-md-4 col-sm-6">
+        <div class="jobint">
+          <div class="row">
+            @foreach($jobs as $job)
+                <div class="col-md-9 col-sm-9">
+                  <h4><a href="{{ route('job.detail.frontend',['slug' => $job->slug]) }}">{{ $job->title }}</a></h4>
+                  <div class="company"><a href="##.">Datebase Management Company</a></div>
+                  <div class="jobloc"><label class="fulltime">Full Time</label>  - <span>New York</span></div>
+                </div>
+            @endforeach    
+          </div>
+        </div>
+      </li>
+      <!--Job end--> 
+    </ul>
+   
+  </div>
+</div>
+<!-- Latest Jobs ends --> 
+
+
+<script type="application/javascript"> 
+</script>
+
 @endsection
