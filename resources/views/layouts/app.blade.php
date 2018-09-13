89d515d0c43278894ab2514fce347524dc28510e
chiranyarao <chiranya.rao@gmail.com>
2018-09-13 12:25:24 +0545

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,19 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    
+<!-- Owl carousel -->
+<link href="{{ asset('public/bundles/css/owl.carousel.css') }}" rel="stylesheet">
+
+<!-- Bootstrap -->
+<link href="{{ asset('public/bundles/css/bootstrap.min.css')}}" rel="stylesheet">
+
+<!-- Font Awesome -->
+<link href="{{ asset('/bundles/css/font-awesome.css')}}" rel="stylesheet">
+
+<!-- Custom Style -->
+<link href="{{ asset('/bundles/css/main.css')}}" rel="stylesheet">
+
     <style>
         .popup-content{
             text-align: center;
@@ -30,76 +43,86 @@
 
 </head>
 <body>
+   
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Capstone') }}
-                </a>
-                <button class="navbar-toggler"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <a class="navbar-brand" href="{{ url('/') }}"> 
+                        <div class="col-md-2 col-sm-3 col-xs-12">
+                            <img src="{{ asset('bundles/images/logo.png') }}" alt="">
+                        </div>
+                    </a>
+                    <button class="navbar-toggler"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="false"
+                            aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
+                    <div class="col-md-10 col-sm-12 col-xs-12"> 
+                        <div class="navbar navbar-default" role="navigation">
+                         <div class="navbar-collapse collapse" id="nav-main">
+                        <!-- Right Side Of Navbar -->
+                            <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                                <li class="dropdown active"><a href="#">Home </a> </li>
+                                <li class="postjob">
+                                    <a href="#" data-toggle="modal" data-target="#loginModal">
+                                        Login
+                                    </a>
+                                </li>
+                                <li class="jobseeker">
+                                    <a href="#"  data-toggle="modal" data-target="#registerModal">
+                                        Register
+                                    </a>
+                                </li>
+                                {{--<li class="nav-item">--}}
+                                    {{--<a class="nav-link" href="{{ route('login') }}">{{ __('Job Seeker Login') }}</a>--}}
+                                {{--</li>--}}
+                                {{--<li class="nav-item">--}}
+                                    {{--<a class="nav-link" href="{{ route('register') }}">{{ __('Job Seeker Register') }}</a>--}}
+                                {{--</li>--}}
+                                {{--<li class="nav-item">--}}
+                                    {{--<a class="nav-link" href="{{ route('admin.login') }}">{{ __('Employer Login') }}</a>--}}
+                                {{--</li>--}}
+                                {{--<li class="nav-item">--}}
+                                    {{--<a class="nav-link" href="{{ route('admin.register.form') }}">{{ __('Employer Register') }}</a>--}}
+                                {{--</li>--}}
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                    </a>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a href="#" class="nav-link" data-toggle="modal" data-target="#loginModal">
-                                    Login
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link" data-toggle="modal" data-target="#registerModal">
-                                    Register
-                                </a>
-                            </li>
-                            {{--<li class="nav-item">--}}
-                                {{--<a class="nav-link" href="{{ route('login') }}">{{ __('Job Seeker Login') }}</a>--}}
-                            {{--</li>--}}
-                            {{--<li class="nav-item">--}}
-                                {{--<a class="nav-link" href="{{ route('register') }}">{{ __('Job Seeker Register') }}</a>--}}
-                            {{--</li>--}}
-                            {{--<li class="nav-item">--}}
-                                {{--<a class="nav-link" href="{{ route('admin.login') }}">{{ __('Employer Login') }}</a>--}}
-                            {{--</li>--}}
-                            {{--<li class="nav-item">--}}
-                                {{--<a class="nav-link" href="{{ route('admin.register.form') }}">{{ __('Employer Register') }}</a>--}}
-                            {{--</li>--}}
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="">{{ __('applied job') }}</a>
+                                        <a class="dropdown-item" href="{{ route('user.profile.update') }}">{{ __('Profile') }}</a>
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                         </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('user.profile.update') }}">{{ __('Profile') }}</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                     </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                        </div>
+                    </div>
+            </div>
                 </div>
+                <div class="clearfix"></div>
             </div>
         </nav>
 
