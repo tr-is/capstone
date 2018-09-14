c898b3ed48d62ff0add95f0465fbf0c657699de9
Kewal Lamichhane <26236473+s3559695@users.noreply.github.com>
2018-09-14 16:12:31 +0545

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
 <!-- search job starts -->
     <div class="searchwrap">
         <div class="container">
-            <h3>We will ensure to find the right path for your career <span>Start your journey today.</span></h3>
+            <h3>One million success stories. <span>Start yours today.</span></h3>
             <form>
                 <div class="searchbar row">
                     <div class="col-md-3">
@@ -30,17 +30,17 @@
                          <input type="text"
                            name="query"
                            class="form-control"
-                           placeholder="Search"
+                           placeholder="Search text"
                            value="{{ request('query') }}"
                            aria-label="Recipient's username"
                            aria-describedby="basic-addon2">
                     </div>
                     <div class="col-md-2">
                       <button class="btn btn-outline-secondary" type="submit">Search Job</button>
-
+                       
                     </div>
                 </div>
-            </form>
+            </form> 
         </div>
     </div>
 
@@ -48,11 +48,10 @@
 
 <!-- How it Works start -->
 <div class="section howitwrap">
-  <div class="container">
+  <div class="container"> 
     <!-- title start -->
     <div class="titleTop">
       <h3>How It <span>Works?</span></h3>
-      <h4>Get started and find your dream job today!</h4>
     </div>
     <!-- title end -->
     <ul class="howlist row">
@@ -60,78 +59,68 @@
       <li class="col-md-4 col-sm-4">
         <div class="iconcircle"><i class="fa fa-user" aria-hidden="true"></i></div>
         <h4>Create An Account</h4>
-        <p>Please Create an account to Proceed.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
       </li>
-      <!--step 1 end-->
-
+      <!--step 1 end--> 
+      
       <!--step 2-->
       <li class="col-md-4 col-sm-4">
         <div class="iconcircle"><i class="fa fa-black-tie" aria-hidden="true"></i></div>
         <h4>Search Desired Job</h4>
-        <p>Once you have created an account. Now you may start to find the desired job your after.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
       </li>
-      <!--step 2 end-->
-
+      <!--step 2 end--> 
+      
       <!--step 3-->
       <li class="col-md-4 col-sm-4">
         <div class="iconcircle"><i class="fa fa-file-text" aria-hidden="true"></i></div>
         <h4>Send Your Resume</h4>
-        <p>It only takes a few seconds, send your resumes now to get the job in line.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
       </li>
       <!--step 3 end-->
     </ul>
   </div>
 </div>
-<!-- How it Works Ends -->
-
-
+<!-- How it Works Ends --> 
 
 <!-- Latest Jobs start -->
 <div class="section greybg">
-  <div class="container">
+  <div class="container"> 
     <!-- title start -->
     <div class="titleTop">
       <h3>Latest <span>Jobs</span></h3>
-      <h4>See what job opportunities we have for you.</h4>
     </div>
     <!-- title end -->
-
+    
     <ul class="jobslist row">
       <!--Job 1-->
-      @foreach($jobs as $job)
+      @foreach($jobs->reverse() as $job)
       <li class="col-md-4 col-sm-6">
-
+      
         <div class="jobint">
-
+        
+        
           <div class="row">
                 <div class="col-md-12 col-sm-12">
                   <h4><a href="{{ route('job.detail.frontend',['slug' => $job->slug]) }}">{{ $job->title }}</a></h4>
-                  <!-- <div class="company">{{$job -> created_by}}</div> -->
-                  <div class="jobloc"><label class="fulltime">$ {{ $job -> salary_range}}</label>   <span>{{ $job -> job_location}}</span></div>
+                  <div class="company">{{ $job->admin ? $job->admin->name : '' }}</div>
+                  <div class="jobloc"><label class="fulltime">$ {{ $job->salary_range}}</label>   <span>{{ $job->job_location}}</span></div>
                 </div>
-
+            
           </div>
-
+              
         </div>
-
+        
       </li>
       @endforeach
-      <!--Job end-->
+      <!--Job end--> 
     </ul>
   </div>
 </div>
-<!-- Latest Jobs ends -->
-
-<!-- How it Works start -->
-<div class="section howitwrap">
-  <div class="container">
-    <!-- title start -->
-    <div class="titleTop">
-      <h3>Connect with your next<span>big hires!</span></h3>
-    </div>
+<!-- Latest Jobs ends --> 
 
 
-<script type="application/javascript">
+<script type="application/javascript"> 
 </script>
 
 @endsection
