c302a873ef420c94458abc1317e9b2d40f1d6fd5
Kewal Lamichhane <26236473+s3559695@users.noreply.github.com>
2018-09-15 17:38:48 +0545

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+t
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Right Job | No. 1 Job Site') }}</title>
+    <title>Right Job | No. 1 Job Site</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
