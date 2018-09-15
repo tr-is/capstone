59a8d142ed185e2dd9b23d1b16ebb32a22d10b8a
chiranyarao <s3602998@student.rmit.edu.au>
2018-09-15 21:49:42 +1000

    homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,10 +37,10 @@
                     </div>
                     <div class="col-md-2">
                       <button class="btn btn-outline-secondary" type="submit">Search Job</button>
-                       
+
                     </div>
                 </div>
-            </form> 
+            </form>
         </div>
     </div>
 
@@ -48,7 +48,7 @@
 
 <!-- How it Works start -->
 <div class="section howitwrap">
-  <div class="container"> 
+  <div class="container">
     <!-- title start -->
     <div class="titleTop">
       <h3>How It <span>Works?</span></h3>
@@ -59,74 +59,74 @@
       <li class="col-md-4 col-sm-4">
         <div class="iconcircle"><i class="fa fa-user" aria-hidden="true"></i></div>
         <h4>Create An Account</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
+        <p></p>
       </li>
-      <!--step 1 end--> 
-      
+      <!--step 1 end-->
+
       <!--step 2-->
       <li class="col-md-4 col-sm-4">
         <div class="iconcircle"><i class="fa fa-black-tie" aria-hidden="true"></i></div>
         <h4>Search Desired Job</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
+        <p></p>
       </li>
-      <!--step 2 end--> 
-      
+      <!--step 2 end-->
+
       <!--step 3-->
       <li class="col-md-4 col-sm-4">
         <div class="iconcircle"><i class="fa fa-file-text" aria-hidden="true"></i></div>
         <h4>Send Your Resume</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
+        <p></p>
       </li>
       <!--step 3 end-->
     </ul>
   </div>
 </div>
-<!-- How it Works Ends --> 
+<!-- How it Works Ends -->
 
 <!-- Latest Jobs start -->
 <div class="section greybg">
-  <div class="container"> 
+  <div class="container">
     <!-- title start -->
     <div class="titleTop">
       <h3>Latest <span>Jobs</span></h3>
     </div>
     <!-- title end -->
-    
+
     <ul class="jobslist row">
       <!--Job 1-->
       @foreach($jobs->reverse() as $job)
       <li class="col-md-4 col-sm-6">
-      
+
         <div class="jobint">
-        
-        
+
+
           <div class="row">
                 <div class="col-md-12 col-sm-12">
                   <h4><a href="{{ route('job.detail.frontend',['slug' => $job->slug]) }}">{{ $job->title }}</a></h4>
                   <div class="company">{{ $job->admin ? $job->admin->name : '' }}</div>
                   <div class="jobloc"><label class="fulltime">$ {{ $job->salary_range}}</label>   <span>{{ $job->job_location}}</span></div>
                 </div>
-            
+
           </div>
-              
+
         </div>
-        
+
       </li>
       @endforeach
-      <!--Job end--> 
+      <!--Job end-->
     </ul>
   </div>
 </div>
-<!-- Latest Jobs ends --> 
+<!-- Latest Jobs ends -->
 <!-- Top Employers start -->
 <div class="section greybg">
-  <div class="container"> 
+  <div class="container">
     <!-- title start -->
     <div class="titleTop">
       <h3>Top <span>Employers</span></h3>
     </div>
     <!-- title end -->
-    
+
     <ul class="employerList">
       <!--employer-->
       <li data-toggle="tooltip" data-placement="top" title="" data-original-title="Company Name"><a href="#"><img src="{{ asset('bundles/images/emplogo1.jpg') }}" alt="Company Name"></a></li>
@@ -161,9 +161,9 @@
     </ul>
   </div>
 </div>
-<!-- Top Employers ends --> 
+<!-- Top Employers ends -->
 
-<script type="application/javascript"> 
+<script type="application/javascript">
 </script>
 
 @endsection
