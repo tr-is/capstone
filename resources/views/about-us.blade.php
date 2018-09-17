c766b7e5d50360a3a986d08df6b3be6b422590aa
chiranyarao <chiranya.rao@gmail.com>
2018-09-17 20:29:55 +1000

    abt

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+ <section>
+    <div >
+
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2 class="uppercase mb8">About Us</h2>
+            </div>
+        </div>
+        <!--end of row-->
+    </div>
+    <!--end of container-->
+</section>
+
+<section  id="about" >
+	<div class="container text-center">
+		<div class="row">
+		  <div >
+		    <h3>RIGHT JOB</h3>
+		    <p>RIGHT JOB is a marketplace that matches job with the world’s most innovative companies. RIGHR JOB combines intelligent job matching with unbiased career counselling to help people find a job they love.
+
+Through Right JOB, job candidates and companies have transparency into salary offers, competing opportunities and job details. Making the recruiting process quicker and more efficient than ever before.
+
+Hired was founded in 2018 and is headquartered in Melbourne, The company is backed by s3559695, s3602998 and other leading investors.
+
+</p>
+		    <!-- <h2 class="classic">The Encounter Team</h2> -->
+            <br><br>
+            
+<!--             http://www.1001fonts.com/rhesmanisa-font.html
+ -->		  </div>
+		</div>
+	</div>
+</section>
+
+
+
+@endsection
