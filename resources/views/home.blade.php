5d59cdbbb21285954b5f202abbade7d9c210bf0a
chiranyarao <chiranya.rao@gmail.com>
2018-09-17 14:12:23 +1000

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('styles')
 @section('content')
     <section class="jobs-category-section" style="height: 100vh;">
         <div class="container">
@@ -18,45 +18,21 @@
                             @endif
     <!-- for each job the title of the job shown in the front page when user logs in -->
                             @if($jobs)
-                            <!--
+
                                <ul class="list-group">
                                     @foreach($jobs as $job)
                                         <li class="list-group-item">
                                             <a href="{{ route('job.detail.frontend',['slug' => $job->slug]) }}">
                                                 {{ $job->title }}
                                             </a>
+                                              <div class="jobloc"><label class="fulltime">$ {{ $job->salary_range}}</label>   <span>{{ $job->job_location}}</span></div>
                                         </li>
 
                                     @endforeach
                                 </ul>
                             @endif
-                          -->
-                          <ul class="jobslist row">
-                            <!--Job 1-->
-                            @foreach($jobs->reverse() as $job)
-                            <li class="col-md-4 col-sm-6">
-
-                              <div class="jobint">
 
 
-                                <div class="row">
-                                      <div class="col-md-12 col-sm-12">
-                                        <h4><a href="{{ route('job.detail.frontend',['slug' => $job->slug]) }}">{{ $job->title }}</a></h4>
-                                        <div class="company">{{ $job->admin ? $job->admin->name : '' }}</div>
-                                        <div class="jobloc"><label class="fulltime">$ {{ $job->salary_range}}</label>   <span>{{ $job->job_location}}</span></div>
-                                      </div>
-
-                                </div>
-
-                              </div>
-
-                            </li>
-                            @endforeach
-                            <!--Job end-->
-                          </ul>
-                              @endif
-                          </div>
-                          </div>
                         </div>
                     </div>
                 </div>
