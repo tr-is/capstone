92b8909e2c8e1ce11c4bddf712b9a289cc934425
Kewal Lamichhane <26236473+s3559695@users.noreply.github.com>
2018-09-19 22:02:55 +0545

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,56 +1,42 @@
 @extends('layouts.app')
-@section('styles')
+
 @section('content')
-    <section class="jobs-category-section" style="height: 1200px;background-color: rgb(0, 139, 139);">
-        <div style="position: relative;display: block;padding: 1rem;margin-bottom: -2;
-        border: 2px solid rgb(27, 197, 197);background-color: aliceblue;left: auto;width: 960px;
-        margin-right: auto;margin-left: auto;max-width: 960px; height: 1200px">
+    <section class="jobs-category-section" style="height: 100vh;">
+        <div class="container">
 
-            <div style="-webkit-box-pack: center !important;justify-content: center !important;position: relative;display: block;padding: 1rem; width: 954px;flex-wrap: wrap;margin-right: -15px;margin-left: -15px;">
+            <div class="row justify-content-center">
                 <div class="col-md-8">
-                    <div >
-                      <!--card */-->
-    <!-- jobs that are available shown for users -->
-                        <div style="
-    width: 1000px; font-size: x-large; text-align: center; color: rgb(15, 95, 175); font-weight: 900;">Available jobs</div>
+                    <div class="card">
+                        <div class="card-header">Available jobs</div>
 
-                        <div style="width: 1100px; padding: 1.25rem; flex: 1 1 auto;">
+                        <div class="card-body">
                             @if (session('status'))
                                 <div class="alert alert-success" role="alert">
                                     {{ session('status') }}
                                 </div>
                             @endif
-    <!-- for each job the title of the job shown in the front page when user logs in -->
-                            @if($jobs)
 
-                               <ul style="display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;flex-direction: column;
-                               padding-left: 10px;margin-bottom: 0px;background-color: aliceblue;-webkit-margin-before: 1em;-webkit-margin-after: 1em;-webkit-margin-start: 0px;-webkit-margin-end: 0px;-webkit-padding-start: 40px;display: list-item;text-align: -webkit-match-parent;
-                               padding-right: 30px;padding-bottom: 20px;padding-top: 20px; width: 850px;">
-
-
-                                    @foreach($jobs as $job)
-
-
-                                        <li style="padding: 1rem;margin-bottom: -2px;border: 2px solid rgb(27, 197, 197);
-                                        background-color: aliceblue;font-size:large;font-weight: 400; ">
-<!---->             <div style="font-size: x-large">
-
-                                            <a  href="{{ route('job.detail.frontend',['slug' => $job->slug])}}">
-                                                {{ $job->title }}
-                                            </a> </div>
-                                            <br>
-                                              <div class="jobloc"style="color:grey"><label style="color:grey">$ {{ $job->salary_range}}</label> <br>  <span> {{ $job->job_location}} </span></div>
-                                        </li>
-
-                                    @endforeach
-                                </ul>
-                            @endif
-
+                            <ul class="jobslist row">
+                                @foreach($jobs as $job)
+                                <li class="col-md-4 col-sm-6">
+                                    <div class="jobint">
+                                        <div class="row">
+                                            <div class="col-md-12 col-sm-12">
+                                                <h4><a href="{{ route('job.detail.frontend',['slug' => $job->slug]) }}">{{$job->title}}</a></h4>
+                                                <div class="company">{{ $job->admin ? $job->admin->name : '' }}</div>
+                                                <div class="jobloc">
+                                                    <label class="fulltime">$ {{ $job->salary_range}}</label>
+                                                    <label><span>Job Match: {{ $job->match  }}</span></label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                @endforeach
+                            </ul>
                         </div>
                     </div>
                 </div>
-            </div>
-
         </div>
     </section>
 
